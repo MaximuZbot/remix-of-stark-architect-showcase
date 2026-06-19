e61640e8a5499767964f81a93f9a710498bb7ffe
MaximuZbot22 <mohithkanna1@gmail.com>
2026-06-19 15:50:23 +0530

    Update standalone routing, titles, and links

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Index from "./pages/Index";
-import ProjectDetail from "./pages/ProjectDetail";
-import Projects from "./pages/Projects";
 import F1Experience from "./pages/F1Experience";
-import NotFound from "./pages/NotFound";
 import ScrollToHash from "./components/ScrollToHash";
 
 const queryClient = new QueryClient();
@@ -20,18 +16,8 @@ const App = () => (
       <BrowserRouter>
         <ScrollToHash />
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/f1" element={<F1Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/work" element={<Navigate to="/projects" replace />} />
-          <Route path="/services" element={<Navigate to="/#capabilities" replace />} />
-          <Route path="/about" element={<Navigate to="/#about" replace />} />
-          <Route path="/contact" element={<Navigate to="/#contact" replace />} />
-          <Route path="/blog" element={<Navigate to="/" replace />} />
-          <Route path="/blog/:id" element={<Navigate to="/" replace />} />
-          <Route path="/project/:slug" element={<ProjectDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<F1Experience />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
